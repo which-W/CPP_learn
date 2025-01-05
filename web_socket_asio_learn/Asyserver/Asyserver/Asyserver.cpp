b904ac0e82ecbcb754071669a27955f7263cb400
which-W <wengzu.love@gmail.com>
2025-01-05 17:38:38 +0800

    asyserver commit

---
+++ web_socket_asio_learn/Asyserver/Asyserver/Asyserver.cpp
@@ -0,0 +1,15 @@
+﻿#include "CServer.h"
+using namespace std;
+#include <iostream>
+int main()
+{
+	try {
+		boost::asio::io_context  io_context;
+		CServer s(io_context, 10086);
+		io_context.run();
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	boost::asio::io_context io_context;
+}
---
+++ web_socket_asio_learn/Asyserver/Asyserver/const.h
@@ -0,0 +1,4 @@
+#pragma once
+#define HEAD_LENGTH 2
+#define MAX_LENGTH 2*1024
+#define MAX_QUEUE_LEN 1000
